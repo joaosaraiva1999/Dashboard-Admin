a571066204e340bda6780b021c783233386fe8d7
joaosaraiva1999 <joaodnice@gmail.com>
2026-09-16 22:46:34 +0100

    Add dashboard notification bell

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,30 @@ lucide.createIcons();
 
 
 const currentPage = window.location.pathname.split('/').pop();
+const sidebar = document.querySelector('.sidebar');
+const mainSideBarContent = document.querySelector('.sidebar-content')
+const smallSideBarContent = document.querySelector('.sidebar-content-smallver')
+const app = document.querySelector('.app')
+const iconBtn = document.querySelector('.icon-btn')
+const sidebarFooter = document.querySelector('.sidebar-footer')
 
 document.querySelectorAll('.sidebar-links li a').forEach(link => {
   const linkPage = link.getAttribute('href')
 
   if (linkPage === currentPage) {
     link.classList.add('active')
+  }
+})
+
+iconBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('is-collapsed')
+  if (sidebar.classList.contains('is-collapsed')) {
+    mainSideBarContent.style.display = 'none';
+    smallSideBarContent.style.display = 'flex';
+    sidebarFooter.style.display = 'none';
+  } else {
+    mainSideBarContent.style.display = 'flex';
+    smallSideBarContent.style.display = 'none';
+    sidebarFooter.style.display = 'block';
   }
 })
